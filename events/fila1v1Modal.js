ce9040af9645cb1d9db696ff3bb659e1391c9014
secundariaraimundinhaa <secundariaraimundinhaa@gmail.com>
2026-03-24 15:19:50 -0300

    Update fila1v1Modal.js

--- events/fila1v1Modal.js
+++ events/fila1v1Modal.js
@@ -32,7 +32,7 @@ module.exports = {
 
     const canalId = interaction.fields.getTextInputValue('canal_id');
     const modo = interaction.fields.getTextInputValue('modo_fila');
-    const valores = ['100,90', '50,90', '20,90', '10,90', '5,90', '2,90', '1,90'];
+    const valores = ['100,15', '50,15', '20,15', '10,15', '5,15', '2,15', '1,15', '0,65'];
 
     // Botões
     const row = new ActionRowBuilder().addComponents(
